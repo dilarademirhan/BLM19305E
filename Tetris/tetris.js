10c8081d5801097e78e517a217c9742c790ba647
dilarademirhan <72828677+dilarademirhan@users.noreply.github.com>
2020-12-31 01:50:19 +0300

    Update tetris.js

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -7,7 +7,6 @@ const tHeight = canvas.height / scale;
 
 let score = 0;
 
-
 let arena = []
 
 const matrix = [
@@ -49,7 +48,7 @@ const matrix = [
 ]
 
 var img = new Image();
-img.src = 'gray.png'
+img.src = 'gray.PNG'
 
 let m = matrix[Math.floor((Math.random() * 5))] //a random tetromino
 
