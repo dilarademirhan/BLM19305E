1ed7c351010ca8d3a091897fca037666523e3b5e
dilarademirhan <72828677+dilarademirhan@users.noreply.github.com>
2020-12-31 15:52:07 +0300

    Add files via upload

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -2,11 +2,8 @@ const canvas = document.getElementById('background');
 const context = canvas.getContext('2d');
 const scale = 20;
 context.scale(scale, scale);
-const tWidth = canvas.width / scale; 
-const tHeight = canvas.height / scale; 
 
 let score = 0;
-
 
 let arena = []
 
@@ -48,15 +45,15 @@ const matrix = [
     ]
 ]
 
-var img = new Image();
-img.src = 'gray.PNG'
+
 
 let m = matrix[Math.floor((Math.random() * 5))] //a random tetromino
 
+var img = new Image();
+img.src = '../images/gray.PNG'
 
 function drawPieces(position) {
     document.onkeydown = keyControl; //rotating tetrominoes
- 
     m.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value) {
@@ -66,31 +63,59 @@ function drawPieces(position) {
     });
 }
 
-// var music = new sound("music.mp3");
-// music.play()
-// var startSound = new sound("start.wav");
-// startSound.play()
-// var rotateSound = new sound("SFX_PieceRotateLR.ogg");
-// var moveSound = new sound("SFX_PieceMoveLR.ogg");
-// var hardDropSound = new sound("SFX_PieceHardDrop.ogg");
-// var clearSound = new sound("SFX_PieceHardDrop.ogg");
+
+var music = new sound("../sounds/music.mp3");
+var startSound = new sound("../sounds/start.wav");
+startSound.play()
+var rotateSound = new sound("../sounds/SFX_PieceRotateLR.ogg");
+var moveSound = new sound("../sounds/SFX_PieceMoveLR.ogg");
+var hardDropSound = new sound("../sounds/SFX_PieceHardDrop.ogg");
+var clearSound = new sound("../sounds/line.wav");
+var gameOverSound = new sound("../sounds/lose.wav");
 
 
-// function sound(src) {
-//     this.sound = document.createElement("audio");
-//     this.sound.src = src;
-//     this.sound.setAttribute("preload", "auto");
-//     this.sound.setAttribute("controls", "none");
-//     this.sound.style.display = "none";
-//     document.body.appendChild(this.sound);
-//     this.play = () => this.sound.play();
-//     this.stop = () => this.sound.pause();   
-// }
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = () => this.sound.play();
+    this.stop = () => this.sound.pause();   
+}
+
+let musicControl = 1
+document.getElementById("music").onclick = function () {
+   musicControl = 0
+   music.stop()
+   document.getElementById("music").style.display = "none";
+   document.getElementById("musicoff").style.display = "inline";
+};
+
+let soundControl = 1
+document.getElementById("sound").onclick = function () {
+    soundControl = 0
+    document.getElementById("sound").style.display = "none";
+    document.getElementById("soundoff").style.display = "inline";
+ };
+ document.getElementById("musicoff").onclick = function () {
+    musicControl = 1 
+    document.getElementById("musicoff").style.display = "none";
+    document.getElementById("music").style.display = "inline";
+ };
+ document.getElementById("soundoff").onclick = function () {
+     soundControl = 1
+     document.getElementById("soundoff").style.display = "none";
+     document.getElementById("sound").style.display = "inline";
+  };
+
 function keyControl(e) {
 
     e = e || window.event;
     if (e.keyCode == '38') {
-        //rotateSound.play()
+        if(soundControl)
+            rotateSound.play()
         m = m[0].map((val, index) => m.map(row => row[index]).reverse())
         if(collision()){
             for (let index = 0; index < 3; index++) {
@@ -99,29 +124,35 @@ function keyControl(e) {
         }
     }
     else if (e.keyCode == '40') {
-        //moveSound.play()
+        if(soundControl)
+            moveSound.play()
         position.y++;
         counter = 0;
     }
     else if (e.keyCode == '37') {
-        //moveSound.play()
+        if(soundControl)
+            moveSound.play()
         position.x--;
         if (collision())
             position.x++;
     }
     else if (e.keyCode == '39') {
-        //moveSound.play()
+        if(soundControl)
+            moveSound.play()
         position.x++;
         if (collision())
             position.x--;
     }else if (e.keyCode === 32) {
-            //hardDropSound.play()
-            period = 0; //1
+        if(soundControl)
+            hardDropSound.play()
+        period = 1; 
     }
 
 }
 
 document.getElementById("up").onclick = function () {
+    if(soundControl)
+        rotateSound.play()
     m = m[0].map((val, index) => m.map(row => row[index]).reverse())
     if(collision()){
         for (let index = 0; index < 3; index++) {
@@ -130,26 +161,33 @@ document.getElementById("up").onclick = function () {
     }
 };
 document.getElementById("down").onclick = function () {
+    if(soundControl)
+        moveSound.play()
     position.y++;
     counter = 0;
 };
 document.getElementById("left").onclick = function () {
+    if(soundControl)
+        moveSound.play()
     position.x--;
     if (collision())
         position.x++;
 };
 document.getElementById("right").onclick = function () {
+    if(soundControl)
+        moveSound.play()
     position.x++;
     if (collision())
         position.x--;
 };
 document.getElementById("space").onclick = function () {
+    if(soundControl)
+        hardDropSound.play()
     period = 0;
 };
 
 
 function collision() {
-    
     for (let i = 0; i < m.length; i++) {
         for (let j = 0; j < m[i].length; j++) {
             if (m[i][j] && arena[position.y + i + 1][position.x + j + 1]){
@@ -159,6 +197,7 @@ function collision() {
     }
     return 0;
 }
+
 function merge() {
     for (let i = 0; i < m.length; i++) {
         for (let j = 0; j < m[i].length; j++) {
@@ -168,7 +207,7 @@ function merge() {
 }
     
 function clearBlocks() {
-    for (let i = 1; i < arena.length-2; i++) {
+    for (let i = 1; i < arena.length - 2; i++) {
         let clear = 1;
 
         for (let j = 1; j < arena[i].length-1; j++) {
@@ -177,18 +216,20 @@ function clearBlocks() {
         }
 
         if (clear) {
-            let r = new Array(tWidth).fill(0);
+            let r = new Array(canvas.width / scale).fill(0);
             r.push(1);
             r.unshift(1);
             score += 100;
             arena.splice(i, 1);
             arena.splice(1, 0, r);
+            if(soundControl)
+                clearSound.play()
         }
     }
 }
 
 function drawArena() {
-    for (let i = 1; i < arena.length-2; i++) {
+    for (let i = 1; i < arena.length - 2; i++) {
         for (let j = 1; j < arena[i].length-1; j++) {
             if (arena[i][j]) {
                 context.drawImage(img, j-1, i-1, 1, 1);
@@ -200,11 +241,11 @@ function drawArena() {
 function initArena() {
     arena = [];
 
-    const r = new Array((canvas.width / 20) + 2).fill(1);
+    const r = new Array((canvas.width / scale) + 2).fill(1);
     arena.push(r);
 
-    for (let i = 0; i < (canvas.height / 20); i++) {
-        let row = new Array(canvas.width / 20).fill(0);
+    for (let i = 0; i < (canvas.height / scale); i++) {
+        let row = new Array(canvas.width / scale).fill(0);
         row.push(1);
         row.unshift(1);
 
@@ -217,7 +258,7 @@ function initArena() {
 
 
 function gameOver() {
-    for (let j = 1; j < arena[1].length-1; j++)
+    for (let j = 1; j < arena[1].length - 1; j++)
         if (arena[1][j]){
             endGame();
             return initArena();
@@ -225,6 +266,10 @@ function gameOver() {
     return;
 }
 function endGame() {
+    if(soundControl)
+        gameOverSound.play()    
+    music.stop()
+
     document.getElementById("background").style.display = 'none';
     document.getElementById("score").style.display = 'none';
     document.getElementById("up").style.display = 'none';
@@ -232,13 +277,20 @@ function endGame() {
     document.getElementById("left").style.display = 'none';
     document.getElementById("right").style.display = 'none';
     document.getElementById("space").style.display = 'none';
+    document.getElementById("music").style.display = 'none';
+    document.getElementById("musicoff").style.display = 'none';
+    document.getElementById("sound").style.display = 'none';
+    document.getElementById("soundoff").style.display = 'none';
+    document.getElementById("restart").style.display = 'none';
+    
+
     let para = document.createElement("p");
     para.innerHTML = "GAME OVER...";
-    para.style.font = "bold 50px arial,serif";
+    para.style.font = "bold 50px Algerian";
     para.style.color = "black"
     let scorePara = document.createElement("p");
     scorePara.innerHTML = "Score: " + score;
-    scorePara.style.font = "bold 50px arial,serif";
+    scorePara.style.font = "bold 50px Algerian";
     scorePara.style.color = "black"
     let btn = document.createElement("button");   
     btn.innerHTML = "Play Again";       
@@ -266,7 +318,9 @@ function endGame() {
 }
 	
 
-
+document.getElementById("restart").onclick = function () {
+    location.reload();
+};
 
 let counter = 0;
 let period = 500;
@@ -274,7 +328,8 @@ let lastTime = 0;
 
 function update(time = 0) {
     document.getElementById('score').innerText = score; 
-    //music.play()
+    if(musicControl)
+        music.play()
     const delta = time - lastTime;
     lastTime = time;
     counter += delta;
@@ -302,8 +357,6 @@ function update(time = 0) {
     drawPieces(position);
     requestAnimationFrame(update);
 }
-
-
 
 
 let position = {
